225a235cce5a05058ce62f70b603b95daacbef34
reissefer <reissefer@gmail.com>
2015-04-07 19:58:15 +0000

    file structure initiated

---
+++ js/application.js
@@ -0,0 +1,10 @@
+/**
+* Created with Solihull College Apprenticeship module choice.
+* User: reissefer
+* Date: 2015-04-07
+* Time: 07:57 PM
+* To change this template use Tools | Templates.
+*/
+define(function() {
+return {};
+});
